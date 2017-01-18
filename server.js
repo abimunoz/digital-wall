d86752047e0440376cdab3b82c2edeb6b0e73ce7
Abi Munoz <abimunozr@gmail.com>
2017-01-17 23:14:42 -0800

    installs hbs

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 var express = require('express');
-var mongo = require('mongodb').MongoClient;
-var handlebars = require('handlebars');
+// var mongo = require('mongodb').MongoClient;
+var hbs = require('hbs');
 var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/sandbox';
 
 
 var app = express();
-
-
-  ////////////////
- // MIDDLEWARE //
-////////////////
+//
+//
+//   ////////////////
+//  // MIDDLEWARE //
+// ////////////////
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'hbs');
-
-
-  ////////////////
- //   CREATE   //
-////////////////
-// app.get('/', function(req, res) {
-//   mongo.connect(url, function(err, db) {
 //
-//   })
-// });
+//
+//   ////////////////
+//  //   CREATE   //
+// ////////////////
+app.get('/', function(request, response, next) {
+  // render the index.hbs template and replace {{title}} with 'MongoDB - Basics'
+  response.render('index', {title: 'MongoDB - Basics'});
+});
 
-  ////////////////
- //    PORT    //
-////////////////
+//
+//   ////////////////
+//  //    PORT    //
+// ////////////////
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Listening on' + port);
